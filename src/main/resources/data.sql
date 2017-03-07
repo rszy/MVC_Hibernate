483e5cf31859d25ed6a6ba4290b23f3a8eb7db71
krzychol99 <krzysztof.odachowski@gmail.com>
2017-03-07 18:16:10 +0100

    Dodanie nowego filmu

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,9 @@
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (1, 'Dicaprio', 'ROMANTIC', '2015-11-12 00:00:00', 'Titanic');
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (2, 'Deep', 'ACTION', '2015-11-13 00:00:00', 'Piraci z Karaibow');
+INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (3, 'Stalone', 'ACTION', '1986-05-09 00:00:00', 'Rambo');
 
 
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (1, 'Marcin', '7', 'Super!', 1);
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (2, 'Basia', '3', 'Kiepski', 1);
+
+I
