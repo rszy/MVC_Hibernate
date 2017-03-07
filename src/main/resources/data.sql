f11236db73c4fbdbeafdda567e0b7f23d2f6f488
Gosia B <a@gmail.com>
2017-03-07 18:13:21 +0100

    dodanie filmu Psy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (1, 'Dicaprio', 'ROMANTIC', '2015-11-12 00:00:00', 'Titanic');
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (2, 'Deep', 'ACTION', '2015-11-13 00:00:00', 'Piraci z Karaibow');
-INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (2, 'Linda', 'ACTION', '2015-11-13 23:00:00', 'Psy');
+INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (3, 'Linda', 'ACTION', '2015-11-13 23:00:00', 'Psy');
 
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (1, 'Marcin', '7', 'Super!', 1);
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (2, 'Basia', '3', 'Kiepski', 1);
+INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (3, 'Gosia', '9', 'Kiepski', 1);
