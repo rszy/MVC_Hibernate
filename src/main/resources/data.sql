33667a36b82abb019d2a3c7c30a803b669187a9f
Gosia B <a@gmail.com>
2017-03-07 19:07:12 +0100

    poprawka po merge Kasi

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (1, 'Dicaprio', 'ROMANTIC', '2015-11-12 00:00:00', 'Titanic');
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (2, 'Deep', 'ACTION', '2015-11-13 00:00:00', 'Piraci z Karaibow');
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (3, 'Linda', 'ACTION', '2015-11-13 23:00:00', 'Psy');
-INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (3, 'Reeves', 'SCI-FI', '2015-10-19 00:00:00', 'Matrix');
-INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (2, 'Streep', 'COMEDY', '2015-11-13 00:00:00', 'Diabeł ubiera się u Prady');
+INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (4, 'Reeves', 'SCI-FI', '2015-10-19 00:00:00', 'Matrix');
+INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (5, 'Streep', 'COMEDY', '2015-11-13 00:00:00', 'Diabeł ubiera się u Prady');
 
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (1, 'Marcin', '7', 'Super!', 1);
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (2, 'Basia', '3', 'Kiepski', 1);
