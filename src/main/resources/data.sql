52494f1914c655186b09979688d27036e4463356
Ryszard Szymanski <ryszymanski@gmail.com>
2017-03-07 18:17:01 +0100

    Dodano nowe dane testowe

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (1, 'Dicaprio', 'ROMANTIC', '2015-11-12 00:00:00', 'Titanic');
 INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (2, 'Deep', 'ACTION', '2015-11-13 00:00:00', 'Piraci z Karaibow');
-
+INSERT INTO movie (id, actors, category, premiere_date, title) VALUES (3, 'Anne Hathaway', 'OTHER', '2015-11-13 00:00:00', 'Diabel ubiera sie u Prady');
 
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (1, 'Marcin', '7', 'Super!', 1);
 INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (2, 'Basia', '3', 'Kiepski', 1);
+INSERT INTO comment (id, nick, rating, text, movie_id) VALUES (3, 'Kasia', '3', 'Fajny', 3);
