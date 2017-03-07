6fc9d2a0212544bf6330d61ccd094f247dd7fabe
Ryszard Szymanski <ryszymanski@gmail.com>
2017-03-07 19:54:40 +0100

    Zmiana nazwy

--- src/main/java/pl/javastart/minifilmweb/CommentController.java
+++ src/main/java/pl/javastart/minifilmweb/CommentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class CommnetController {
+public class CommentController {
 
     @Autowired
     private MovieRepository movieRepository;
